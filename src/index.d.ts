73e06c2612a22b99c67c047edc6cbf28afc38963
Philippe Assis <codephilippe@gmail.com>
2023-05-29 11:19:07 -0300

    type definition typescriot

---
+++ src/index.d.ts
@@ -0,0 +1,124 @@
+declare module 'fastapi' {
+  import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+
+  interface FastAPIOptions {
+    model?: string;
+    forceCreateTables?: boolean;
+    database?: {
+      database?: string;
+      username?: string;
+      password?: string;
+      sync?: 'alter' | 'force';
+    };
+  }
+
+  interface RouteOptions {
+    tags?: string[];
+    summary?: string;
+    description?: string;
+    responses?: {
+      [statusCode: number]: {
+        description: string;
+        content: {
+          [contentType: string]: {
+            schema: {
+              type: string;
+              properties: {
+                [propertyName: string]: {
+                  type: string;
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  }
+
+  type RequestHandler = (
+    request: FastifyRequest,
+    reply: FastifyReply
+  ) => Promise<any>;
+
+  class FastAPI {
+    constructor(options?: FastAPIOptions);
+    routes: {
+      paths: {
+        [path: string]: {
+          [method: string]: RouteOptions;
+        };
+      };
+      handler: RequestHandler;
+    }[];
+    tags: {
+      [name: string]: string[];
+    };
+    handlers: {
+      [name: string]: RequestHandler;
+    };
+    model: string | null;
+    models: {
+      [name: string]: any;
+    };
+    database: {
+      database: string | null;
+      username: string | null;
+      password: string | null;
+      sync: 'alter' | 'force' | null;
+    };
+    cors: {
+      origin: string;
+    };
+
+    load(callback?: (err?: Error) => void): void;
+    builder(
+      routes: {
+        paths: {
+          [path: string]: {
+            [method: string]: RouteOptions;
+          };
+        };
+        handler: RequestHandler;
+      }[],
+      tags: {
+        [name: string]: string[];
+      },
+      handlers: {
+        [name: string]: RequestHandler;
+      }
+    ): void;
+    defaultListen(err: Error, address: string): void;
+    listen(callback?: (err?: Error) => void): void;
+    start(callback?: (err?: Error) => void): void;
+    setDataBase(database: {
+      database?: string;
+      username?: string;
+      password?: string;
+    }): FastAPI;
+    setModel(model: string): FastAPI;
+    addRoutes(
+      name: string,
+      specMethods: RouteOptions,
+      handler: RequestHandler
+    ): FastAPI;
+    addRoute(
+      path: string,
+      method: string,
+      specMethod: RouteOptions,
+      handler: RequestHandler
+    ): FastAPI;
+    get(path: string, spec: RouteOptions, handler: RequestHandler): FastAPI;
+    post(path: string, spec: RouteOptions, handler: RequestHandler): FastAPI;
+    put(path: string, spec: RouteOptions, handler: RequestHandler): FastAPI;
+    delete(path: string, spec: RouteOptions, handler: RequestHandler): FastAPI;
+    patch(path: string, spec: RouteOptions, handler: RequestHandler): FastAPI;
+    setHandler(name: string, handler: RequestHandler): FastAPI;
+    setTags(name: string, tags: string[]): FastAPI;
+    getModels(): { [name: string]: any };
+  }
+
+  const fastapi: FastAPI;
+  const listen: FastifyInstance['listen'];
+
+  export { fastapi, listen, FastAPI };
+}
